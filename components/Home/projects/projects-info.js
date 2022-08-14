6041483bcfe29d3c1f05c7cb98513fed8fb75a61
MrRobz <robinphilipthomas@outlook.in>
2022-08-14 23:25:05 +0530

    update project info

--- components/Home/projects/projects-info.js
+++ components/Home/projects/projects-info.js
@@ -1,25 +1,27 @@
 import iconShelfCover from "../../../public/icon-shelf.png";
+import priceRounderCover from "../../../public/price-rounder-99.avif";
+import peerAppreciationCardsCover from "../../../public/peer-appreciation-cards.png";
 
 export const ProjectsInfoList = [
   {
-    title: "Crono: Productivity tool for techies to stay informed",
-    desc: "Turned an idea into a successful product that helps techies with long work hours schedules to stay updated and productive. I worked on the MVP and improved the product experience as Crono gained users.",
-    tags: ["Mobile app", "Web extension", "Desktop app"],
+    title: "Icon Shelf: SVG icon organizer for web devs",
+    desc: "Icon Shelf is a free and open-source SVG icon manager for web developers. Improve how you add icons to your frontend project. All your icons in one place, easily copyable as code.",
+    tags: ["Desktop app", "React", "Electron"],
     cover: iconShelfCover,
     link: "https://icon-shelf.github.io",
   },
   {
-    title: "Crono: Productivity tool for techies to stay informed",
-    desc: "Turned an idea into a successful product that helps techies with long work hours schedules to stay updated and productive. I worked on the MVP and improved the product experience as Crono gained users.",
-    tags: ["Mobile app", "Web extension", "Desktop app"],
-    cover: iconShelfCover,
-    link: "https://icon-shelf.github.io",
+    title: "Price Rounder 99: Round-off price tags",
+    desc: "Price Rounder 99 chrome extension that rounds off  the prices for you and shows the rounded price on your screen. So you can take a better decisions while online shopping.",
+    tags: ["Chrome extension", "Javascript"],
+    cover: priceRounderCover,
+    link: "https://www.producthunt.com/products/launchaco#price-rounder-99",
   },
   {
-    title: "Crono: Productivity tool for techies to stay informed",
-    desc: "Turned an idea into a successful product that helps techies with long work hours schedules to stay updated and productive. I worked on the MVP and improved the product experience as Crono gained users.",
-    tags: ["Mobile app", "Web extension", "Desktop app"],
-    cover: iconShelfCover,
-    link: "https://icon-shelf.github.io",
+    title: "Peer appreciation cards: generate appreciation cards",
+    desc: "Take a moment to celebrate your peers, co-workers and leaders for the great work they are doing. Write a note of appreciation to them for championing company culture values",
+    tags: ["Web app", "Emberjs"],
+    cover: peerAppreciationCardsCover,
+    link: "https://peer-appreciation-cards.netlify.app/",
   },
 ];
