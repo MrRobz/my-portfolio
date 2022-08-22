8b5623160c4a11ab1045ae184fb6555ff0b0333e
MrRobz <robinphilipthomas@outlook.in>
2022-08-23 00:41:11 +0530

    add social share meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,26 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Epilogue:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
+
+          {/* <!-- Open Graph / Facebook --> */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://www.robie.dev/" />
+          <meta property="og:title" content="Robin Philip Thomas" />
+          <meta
+            property="og:description"
+            content="Robin Philip Thomas's portfolio website."
+          />
+          <meta property="og:image" content="/social-share-img.png" />
+
+          {/* <!-- Twitter --> */}
+          <meta property="twitter:card" content="/social-share-img.png" />
+          <meta property="twitter:url" content="https://www.robie.dev/" />
+          <meta property="twitter:title" content="Robin Philip Thomas" />
+          <meta
+            property="twitter:description"
+            content="Robin Philip Thomas's portfolio website."
+          />
+          <meta property="twitter:image" content="/social-share-img.png" />
         </Head>
         <body>
           <Main />
